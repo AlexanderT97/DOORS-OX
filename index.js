0971fcaad9002901b0556816db486f2529f4dc63
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2025-05-02 14:01:43 +0200

    port update

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-/*const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+/*app.listen(PORT, () => {
     console.log(`Server läuft auf http://localhost:${PORT}`);
 });*/
 
